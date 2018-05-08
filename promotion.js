cbfff54ba5542bf9e2635b1a1f18e40e875f834d
Colton Loewen <loewencjay@gmail.com>
2018-05-08 17:25:31 -0600

    Added authentication

--- promotion.js
+++ promotion.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+process.env.REQUIREMENTS = "cjcool:SECRET@494072,rank1:5,rank2:10;cjcoolOther:SECRET2@3599691,role1:15";
 
+const TOKEN = process.env.TOKEN;
+
+router.use((req, res, next) => {
+	if (req.query.token === TOKEN) {
+		next();
+	} else {
+		res.status(401).end();
+	}
+});
+
+router.get("/requirements", (req, res) => {
+	res.send("authenticated!");
+});
 
 module.exports = router;
